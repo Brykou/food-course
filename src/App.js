1cf5c0545f1b00d8c7730e61dbdd90796c33f59b
Brykou <brice.leydet@webedia-group.com>
2018-10-12 20:07:13 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="app">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
